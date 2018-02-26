3506eccaab145e33d57cc1761822a8e6069afb31
nolanlee4 <nolanlee4@outlook.com>
2018-02-26 17:49:05 -0500

    Update excel-configlet-builder.py

--- excel-reader/excel-configlet-builder.py
+++ excel-reader/excel-configlet-builder.py
@@ -1,13 +1,14 @@
 #import pyexcel to read excel sheets
 import pyexcel
+from cvplibrary import Form
+from cvplibrary import CVPGlobalVariables,GlobalVariableNames
 
 #build dictionary of excel sheet
-#my_array = pyexcel.get_array(file_name="testcsv.xlsx")
-my_dict = pyexcel.get_dict(file_name="testcsv.xlsx")
+my_dict = pyexcel.get_dict(file_name="/opt/CVPfiles/cutsheet.xlsx")
 
 # print '%s' % (my_array)
 
-print my_dict
+#print my_dict
 #define variables from keys of dictionary
 systemId = my_dict['systemId']
 ip_address = my_dict['ipAddress']
@@ -18,7 +19,7 @@ print
 # print systemId[0]
 
 #get user input from form, to be added
-user_input = "test2"
+user_input = Form.getFieldById('systemId').getValue()
 
 device_index = systemId.index(user_input)
 
